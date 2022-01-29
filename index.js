ae5fed14aad85f3745e975f9bd8a4c24c5e66578
Ben Myers <ben.myers@okstate.edu>
2022-01-29 10:32:48 -0600

    Helper for sending messages to client

--- index.js
+++ index.js
@@ -4,35 +4,6 @@ require('dotenv').config();
 // const searchForBlogPost = require('./commands/blog');
 const searchForStream = require('./commands/stream');
 
-/**
- * @callback Command
- * @param {import('tmi.js').Client} client - Twitch chat client
- * @param {string} command - triggered command
- * @param {string} body - message body after the command
- */
-
-/** @type {Object<string, Command>} */
-const commands = {
-	// Simple call-and-response commands
-	discord: client => client.say('Looking for inclusive web development communities online? Join the Lunch Dev Discord (https://discord.gg/lunchdev) and the Frontend Horse Discord (https://frontend.horse/chat)!'),
-	theme: client => client.say('The VS Code theme is Night Mind, by @b1mind! Check it out at https://marketplace.visualstudio.com/items?itemName=b1m1nd.night-mind'),
-	uses: client => client.say(`Check out Ben's whole setup at https://benmyers.dev/uses/!`),
-
-	// More complicated commands
-	// blog: searchForBlogPost,
-	stream: searchForStream
-};
-
-/** @type {Object<string, Command>} */
-const moderatorCommands = {
-	// Simple call-and-response commands
-	frontendhorse: client => client.say('Join the Frontend Horse Discord! https://frontendhorse.chat'),
-	lunchdev: client => client.say('Join the Lunch Dev Discord server! https://discord.gg/lunchdev'),
-	reactpodcast: client => client.say('Join the Lunch Dev Discord server! https://discord.gg/lunchdev'),
-
-	// More complicated commands
-};
-
 const client = new tmi.Client({
 	connection: {
 		secure: true,
@@ -46,6 +17,39 @@ const client = new tmi.Client({
 });
 
 client.connect();
+
+function say(...args) {
+	client.say(process.env.TWITCH_BROADCASTER_USERNAME, ...args);
+}
+
+/**
+ * @callback Command
+ * @param {import('tmi.js').Client} client - Twitch chat client
+ * @param {string} command - triggered command
+ * @param {string} body - message body after the command
+ */
+
+/** @type {Object<string, Command>} */
+const commands = {
+	// Simple call-and-response commands
+	discord: () => say('Looking for inclusive web development communities online? Join the Lunch Dev Discord (https://discord.gg/lunchdev) and the Frontend Horse Discord (https://frontend.horse/chat)!'),
+	theme: () => say('The VS Code theme is Night Mind, by @b1mind! Check it out at https://marketplace.visualstudio.com/items?itemName=b1m1nd.night-mind'),
+	uses: () => say(`Check out Ben's whole setup at https://benmyers.dev/uses/!`),
+
+	// More complicated commands
+	// blog: searchForBlogPost,
+	stream: searchForStream
+};
+
+/** @type {Object<string, Command>} */
+const moderatorCommands = {
+	// Simple call-and-response commands
+	frontendhorse: () => say('Join the Frontend Horse Discord! https://frontendhorse.chat'),
+	lunchdev: () => say('Join the Lunch Dev Discord server! https://discord.gg/lunchdev'),
+	reactpodcast: () => say('Join the Lunch Dev Discord server! https://discord.gg/lunchdev'),
+
+	// More complicated commands
+};
 
 const COMMAND_REGEX = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
