1a440843e391f6e47788d4349100753ecc4d377d
Ben Myers <ben.myers@okstate.edu>
2022-02-04 22:01:47 -0600

    Add some supplemental statements for shouting folks out

--- commands/so.js
+++ commands/so.js
@@ -1,21 +1,56 @@
 const twitchApi = require('../utils/twitch');
 
-/** @type {Object<string, string>} */
-const names = {
-	'5t3phdev': 'Steph',
-	ajcwebdev: 'Anthony',
-	buildingbedrocklayout: 'Travis',
-	fimion: 'Alex',
-	genericmikechen: 'Mike',
-	geometricjim: 'Jim',
-	jlengstorf: 'Jason',
-	kevinpowellcss: 'Kevin',
-	lunchdev: 'Michael',
-	mannimoki: 'Manny',
-	maxcellw: 'Prince',
-	peruvianidol: 'Mike',
-	trostcodes: 'Alex',
-	whitep4nth3r: 'Salma',
+/** @type {Object<string, {name: string, additionalMessage?: string}>} */
+const extraDetails = {
+	'5t3phdev': {
+		name: 'Steph',
+		additionalMessage: `Steph's streams are a great place to learn about CSS, Eleventy, and more!`
+	},
+	ajcwebdev: {
+		name: 'Anthony'
+	},
+	buildingbedrocklayout: {
+		name: 'Travis',
+		additionalMessage: `Travis is streaming his progress building out the Bedrock Layout component library.`
+	},
+	fimion: {
+		name: 'Alex'
+	},
+	genericmikechen: {
+		name: 'Mike',
+		additionalMessage: `Mike's practice frontend dev interviews are a great resource for any frontend devs looking for their next job!`
+	},
+	geometricjim: {
+		name: 'Jim',
+		additionalMessage: `Jim's streams are an invaluable resources for learning accessibility tips.`
+	},
+	jlengstorf: {
+		name: 'Jason',
+		additionalMessage: `Jason's show Learn With Jason is a great way to learn more about building for the Jamstack!`
+	},
+	kevinpowellcss: {
+		name: 'Kevin',
+	},
+	lunchdev: {
+		name: 'Michael',
+	},
+	mannimoki: {
+		name: 'Manny',
+	},
+	maxcellw: {
+		name: 'Prince',
+	},
+	peruvianidol: {
+		name: 'Mike',
+		additionalMessage: `Mike holds office hours for CSS, Eleventy, and more each Friday.`
+	},
+	trostcodes: {
+		name: 'Alex',
+		additionalMessage: `Alex's Frontend Horse streams with guests from around web development and web design are a great way to dive into creative coding!`
+	},
+	whitep4nth3r: {
+		name: 'Salma',
+	}
 };
 
 /**
@@ -56,10 +91,15 @@ async function getLatestStream(username) {
 
 		const displayName = await getDisplayName(streamer);
 		const url = `https://twitch.tv/${displayName}`;
-		const name = names[streamer.toLowerCase()] || `@${displayName}`;
+		const streamerDetails = extraDetails[streamer.toLowerCase()];
+		const name = streamerDetails?.name || `@${displayName}`;
 		const sentences = [
 			`Go check out @${displayName} at ${url}!`
 		];
+
+		if (streamerDetails?.additionalMessage) {
+			sentences.push(streamerDetails.additionalMessage);
+		}
 
 		const latestStream = await getLatestStream(streamer);
 		if (latestStream) {
